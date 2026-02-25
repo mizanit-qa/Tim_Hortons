e286005526b2cb0a0b099a9cbb1638ef240ea095
itaghiyev <110480452+itaghiyev@users.noreply.github.com>
2026-02-25 16:44:34 -0500

    TC-006 updates

--- tests/TC-006_Homepage-Menu.spec.js
+++ tests/TC-006_Homepage-Menu.spec.js
@@ -2,20 +2,23 @@ import { test, expect } from '@playwright/test';
 import { ProtectedPage } from './pages/ProtectedPage';
 import { SignInPage } from './pages/SignInPage';
 import { HomePage } from './pages/HomePage';
+import { LocationsPage } from './pages/LocationsPage';
 
 test('Homepage - Menu', async ({ page }) => {
     const sitepass = new ProtectedPage(page);
     const signin = new SignInPage(page);
     const homepage = new HomePage(page);
+    const location = new LocationsPage(page);
 
     await sitepass.passwordProtection({ timeout: 5000 });
 
     await signin.gotoHome();
+
     await signin.userSignIn('timregression+95@gmail.com');
 
     await homepage.homepageMenu();
 
-
+    await location.storeSelection();
 
 })
 
--- tests/pages/LocationsPage.js
+++ tests/pages/LocationsPage.js
@@ -2,7 +2,19 @@ exports.LocationsPage = class LocationsPage {
 
     constructor(page) {
         this.page = page;
+        this.yourAddress = page.getByTestId("storelocator-autocomplete");
+        this.storeAccordionBtn = "//div[@id='tabpanel-0']//button[contains(@aria-label,'1500 woodbine ave. Open Accordion')]";
+        this.storeOrderBtn = page.getByTestId("store-action-button-order");
+
+
+
         
+    }
+
+    async storeSelection (){
+        await this.yourAddress.fill("1500 woodbine ave. Open Accordion");
+        await this.storeAccordionBtn.click();
+        await this.storeOrderBtn.click();
     }
 
 
