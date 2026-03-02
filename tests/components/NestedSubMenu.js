6f0fbc0ea72da6b66ee86f1dc9e392514b9dc0d8
Ilgar <ilgartaghi@gmail.com>
2026-03-01 21:06:10 -0500

    Add NestedSubMenu class and integrate into brewed coffee selection test

---
+++ tests/components/NestedSubMenu.js
@@ -0,0 +1,14 @@
+exports.nestedSubMenu = class nestedSubMenu {
+
+    constructor(page) {
+        this.page = page;
+        this.brewedCoffee = page.getByRole('link', { name: /Brewed Coffee/i });
+
+    }
+
+    async clickBrewedCoffee() {
+        await this.brewedCoffee.click();
+    }
+
+
+}
